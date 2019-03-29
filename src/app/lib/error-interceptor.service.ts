a70a35956529cf348523d7584aab12df4af43491
Jorge Fernandez <jorge.fernandez@atmira.com>
2019-03-29 09:20:45 +0100

    Modificado error-interceptor

--- src/app/lib/error-interceptor.service.ts
+++ src/app/lib/error-interceptor.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { StoreService } from './store.service';
+import { Router } from '@angular/router';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class ErrorInterceptorService {
+@Injectable()
+export class ErrorInterceptorService implements HttpInterceptor {
 
-  constructor() { }
+  // Declaración de StoreService y Router privadas para uso de la clase
+  constructor(
+    private _storeService: StoreService,
+    private router: Router
+  ) { }
+  
+  // Método principal de los interceptores. Comprueba si hay error y lo manda a otro método
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const handledRequest = next.handle(req).pipe(tap<HttpEvent<any>>(null, error => {
+      if(error instanceof HttpErrorResponse) {
+        this.errorResponse(error);
+      }else {
+        console.log(error);
+      }
+    }));
+    return handledRequest;
+  }
+
+  // Comprueba si el error es de autenticación para enviarlo al login
+  private errorResponse(error: HttpErrorResponse) {
+    if(error.status == 401) {
+      this._storeService.emitMessage('No autorizado');
+      this.redirectToLogin();
+    }else {
+      console.log(error.statusText);
+    }
+  }
+
+  // Redirección al login
+  private redirectToLogin() {
+    this.router.navigateByUrl('/credentials/login');
+  }
+
 }
--- src/app/lib/lib.module.ts
+++ src/app/lib/lib.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { TokenInterceptorService } from './token-interceptor.service';
+import { ErrorInterceptorService } from './error-interceptor.service';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptorService,
+    multi: true
+  }, {
+    provide: HTTP_INTERCEPTORS,
+    useClass: ErrorInterceptorService,
+    multi: true
+  }]
 })
 export class LibModule { }
